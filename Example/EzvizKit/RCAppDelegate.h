1e710ad5f6b3bc2fe1d5e904b1b8e1125767b118
hujiao <hujiao@ricent.com>
2022-11-01 17:44:54 +0800

    Initial commit

---
+++ Example/EzvizKit/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  EzvizKit
+//
+//  Created by hujiao on 11/01/2022.
+//  Copyright (c) 2022 hujiao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/EzvizKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EzvizKit
+//
+//  Created by hujiao on 11/01/2022.
+//  Copyright (c) 2022 hujiao. All rights reserved.
+//
+
+@import UIKit;
+#import "RCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
